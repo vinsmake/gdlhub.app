965df30d2f49b6659b340d94222a88ab51eccc5c
vinsmake <ps.enrique.e@gmail.com>
2025-02-11 15:23:55 -0600

    Project updated

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const buttonVariants = {
-  default: "bg-black text-white shadow-sm hover:bg-blue-500",
-  primaryActive: "bg-primary text-white shadow",
-  primaryInactive: "bg-primary text-white shadow",
-  destructive: "bg-secondary text-primary shadow-sm hover:bg-red-500",
+  default: "bg-red-500 text-white shadow-sm hover:bg-blue-500",
+  primaryActive: "bg-green-500 text-white shadow hover:bg-blue-500",
+  primaryInactive: "bg-yellow-500 text-white shadow hover:bg-blue-500",
+  destructive: "bg-blue-500 text-primary shadow-sm hover:bg-red-500",
   outline: "border border-gray-300 bg-white text-gray-700 shadow-sm hover:bg-gray-100",
   secondary: "bg-gray-600 text-white shadow-sm hover:bg-gray-500",
   ghost: "hover:bg-gray-100 text-gray-700",
@@ -18,15 +18,14 @@ const sizeVariants = {
   icon: "h-9 w-9 flex items-center justify-center",
 };
 
-const Button = ({ variant = "default", size = "default", className = "", asChild, children, ...props }) => {
-  const Comp = asChild ? "span" : "button"; // Permite envolver en otro elemento si se necesita
+const Button = ({ variant = "default", size = "default", className = "", children, ...props }) => {
   return (
-    <Comp
-      className={`inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 ${buttonVariants[variant]} ${sizeVariants[size]} ${className}`}
+    <button
+      className={`inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 "h-9 px-4 py-2 ${buttonVariants[variant]} ${className}`}
       {...props}
     >
       {children}
-    </Comp>
+    </button>
   );
 };
 
