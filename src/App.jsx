4c8ffca9bb8ce67767498f3305848159cdf6132a
vinsmake <ps.enrique.e@gmail.com>
2025-02-11 16:18:10 -0600

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import Button from "./components/Button/Button"
 
 export const App = () => {
-  return (
+    return (
     <>
-      <div>
-        <Button>Botón por defecto</Button>
-        <Button variant="primaryActive">Eliminar</Button>
-        <Button variant="primaryInactive">
-          <a href="https://ejemplo.com" target="_blank" rel="noopener noreferrer">Ir a Ejemplo</a>
-        </Button>      <Button variant="secondary" size="lg">Botón grande</Button>
-        <Button variant="ghost">Botón ghost</Button>
-        <Button variant="link">Botón link</Button>
+        <div>
+      <Button>Botón por defecto</Button>
+      <Button variant="primaryActive">Eliminar</Button>
+      <Button variant="primaryInactive">Botón de Link</Button>
+      <Button variant="secondary" size="lg">Botón grande</Button>
+      <Button variant="ghost">Botón ghost</Button>
+      <Button variant="link">Botón link</Button>
 
-        {/* Botón como enlace usando asChild */}
-        <Button asChild>
-          <a href="/dashboard">Ir al Dashboard</a>
-        </Button>
-      </div>
+      {/* Botón como enlace usando asChild */}
+      <Button asChild>
+        <a href="/dashboard">Ir al Dashboard</a>
+      </Button>
+    </div>
 
     </>
-  )
+    )
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const buttonVariants = {
-  default: "bg-red-500 text-white shadow-sm hover:bg-blue-500",
-  primaryActive: "bg-green-500 text-white shadow hover:bg-blue-500",
-  primaryInactive: "bg-yellow-500 text-white shadow hover:bg-blue-500",
-  destructive: "bg-blue-500 text-primary shadow-sm hover:bg-red-500",
+  default: "bg-blue-600 text-white shadow-sm hover:bg-blue-600",
+  primaryActive: "bg-gradient-to-r from-red-600 to-red-700 border border-red-800 text-white shadow transition-all duration-300 hover:from-red-700 hover:to-red-800 hover:border-red-900",
+  primaryInactive: "bg-yellow-600 text-white shadow hover:bg-blue-600",
+  destructive: "bg-blue-600 text-primary shadow-sm hover:bg-red-600",
   outline: "border border-gray-300 bg-white text-gray-700 shadow-sm hover:bg-gray-100",
-  secondary: "bg-gray-600 text-white shadow-sm hover:bg-gray-500",
+  secondary: "bg-gray-600 text-white shadow-sm hover:bg-gray-600",
   ghost: "hover:bg-gray-100 text-gray-700",
   link: "text-blue-600 underline-offset-4 hover:underline",
 };
@@ -14,7 +14,7 @@ const buttonVariants = {
 const Button = ({ variant = "default", size = "default", className = "", children, ...props }) => {
   return (
     <button
-      className={`inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 "h-9 px-4 py-2 ${buttonVariants[variant]} ${className}`}
+      className={`inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-3 focus-visible:ring-red-500 h-9 px-4 py-2 ${buttonVariants[variant]} ${className}`}
       {...props}
     >
       {children}
