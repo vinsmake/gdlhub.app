c6caca6b0533d52127d7f5a5430bce0595fa61b5
vinsmake <ps.enrique.e@gmail.com>
2025-03-21 15:10:12 -0600

    Project updated

--- src/ui/DashboardSidebar.jsx
+++ src/ui/DashboardSidebar.jsx
@@ -1,34 +1,44 @@
-export const DashboardSidebar = ({ children }) => {
+import {
+    Home,
+    Utensils,
+    List,
+    Percent,
+    MapPin,
+    Heart,
+    Eye,
+    Settings,
+  } from "lucide-react";
+  
+  export const DashboardSidebar = () => {
     return (
-        <>
-            <aside className="w-1/4 bg-neutral-800 rounded-xl p-4 text-white">
-                <nav className="flex flex-col gap-2">
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Inicio
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Restaurantes
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Categorías
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Ofertas
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Mapa
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Favoritos
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Ajustes
-                    </a>
-                    <a href="#" className="hover:bg-neutral-600 rounded px-3 py-2 transition">
-                        Cerrar sesión
-                    </a>
-                </nav>
-            </aside>
-        </>
-    )
-}
+      <aside className="w-1/5 bg-neutral-900 rounded-xl p-4 text-white text-lg">
+        <nav className="flex flex-col gap-2">
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Home size={24} /> Inicio
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Utensils size={24} /> Restaurantes
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <List size={24} /> Categorías
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Percent size={24} /> Ofertas
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <MapPin size={24} /> Mapa
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Heart size={24} /> Favoritos
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Eye size={24} /> Visitados
+          </a>
+          <a href="#" className="flex items-center gap-2 hover:bg-neutral-600 rounded-xl px-3 py-2 transition">
+            <Settings size={24} /> Configuración
+          </a>
+        </nav>
+      </aside>
+    );
+  };
+  
