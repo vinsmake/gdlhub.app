58fa7da468e481cc33f7b4424ed1633a0b969699
vinsmake <ps.enrique.e@gmail.com>
2025-03-21 10:33:33 -0600

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import Button from "./components/Button/Button"
+import { Screen } from "./ui/Screen"
 
 export const App = () => {
-    return (
+  return (
     <>
-        <div>
-      <Button>Botón por defecto</Button>
-      <Button variant="primaryActive">Eliminar</Button>
-      <Button variant="primaryInactive">Botón de Link</Button>
-      <Button variant="secondary" size="lg">Botón grande</Button>
-      <Button variant="ghost">Botón ghost</Button>
-      <Button variant="link">Botón link</Button>
+      <Screen>
+        <Button>Botón por defecto</Button>
+        <Button variant="primaryActive">Eliminar</Button>
+        <Button variant="primaryInactive">Botón de Link</Button>
+        <Button variant="secondary" size="lg">Botón grande</Button>
+        <Button variant="ghost">Botón ghost</Button>
+        <Button variant="link">Botón link</Button>
 
-      {/* Botón como enlace usando asChild */}
-      <Button asChild>
-        <a href="/dashboard">Ir al Dashboard</a>
-      </Button>
-    </div>
+        {/* Botón como enlace usando asChild */}
+        <Button asChild>
+          <a href="/dashboard">Ir al Dashboard</a>
+        </Button>
+      </Screen>
 
     </>
-    )
+  )
 }
---
+++ src/ui/Screen.jsx
@@ -0,0 +1,8 @@
+export const Screen = ({children }) => {
+    return (
+      <div className={`w-screen h-screen bg-neutral-900`}>
+        {children}
+      </div>
+    );
+  };
+  
